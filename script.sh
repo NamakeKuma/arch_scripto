3810393ad65cb30bb9aa8ca9324f36c414a247d8
NamakeKuma <namakekuma@tuta.io>
2025-05-20 16:57:55 +0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,4 +1,4 @@
-echo "Server = https://mirror.datacenter.by/pub/archlinux/$repo/os/$arch" > /etc/pacman.d/mirrorlist
+echo "Server = https://mirror.datacenter.by/pub/archlinux/\$repo/os/\$arch" > /etc/pacman.d/mirrorlist
 pacman -Syy
 setfont cyr-sun16
 archinstall
